9848f8f9f6b4ecac0e655eb09bd7897fa7a3fe29
itsaryanchauhan1 <itsaryanchauhan1@gmail.com>
2024-09-03 10:00:04 +0530

    gloabal

--- SCRIPTS/global.gd
+++ SCRIPTS/global.gd
@@ -3,6 +3,11 @@ extends Node
 
 var selected_articles = ["", "", "", "", ""]  # Array for the selected articles
 
+
+
+
+
+
 func save_article(article_position, article_text):
 	selected_articles[article_position] = article_text
 
